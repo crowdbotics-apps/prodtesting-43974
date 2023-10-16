769c22e1463be42c1ab9e731bbe83047d448ad29
Mithilesh Tiwari <Mithilesh@crowdbotics.com>
2023-10-17 00:18:56 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,5 +1,3 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "prodtesting-43974",
   url: "https://prodtesting-43974.botics.co",
@@ -7,9 +5,9 @@ export const globalOptions = {
   screenOptions: {
     headerShown: false
   }
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
--- screens/ScreenAI2/index.js
+++ screens/ScreenAI2/index.js
@@ -1,26 +1,27 @@
-import React, { useState } from 'react';
-import { SafeAreaView, StyleSheet, TextInput, Button, Image, Text } from 'react-native';
+import React, { useState } from "react";
+import { SafeAreaView, StyleSheet, TextInput, Button, Image, Text } from "react-native";
 
 const LoginScreen = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   return <SafeAreaView style={styles.container}>
       <Image style={styles.logo} source={{
-      uri: 'https://tinyurl.com/42evm3m3'
+      uri: "https://tinyurl.com/42evm3m3"
     }} />
       <Text style={styles.title}>Welcome Back!</Text>
       <TextInput style={styles.input} onChangeText={setEmail} value={email} placeholder="Email" keyboardType="email-address" />
       <TextInput style={styles.input} onChangeText={setPassword} value={password} placeholder="Password" secureTextEntry />
       <Button title="Login" onPress={() => {}} />
+      <Button title="Save" onPress={() => {}} />
     </SafeAreaView>;
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
   },
   logo: {
     width: 150,
@@ -33,8 +34,8 @@ const styles = StyleSheet.create({
   },
   input: {
     height: 40,
-    width: '80%',
-    borderColor: 'gray',
+    width: "80%",
+    borderColor: "gray",
     borderWidth: 1,
     marginBottom: 20,
     paddingLeft: 10
